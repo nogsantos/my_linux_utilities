059edefcede982c1bc9fa944141cdb51ae528427
Fabricio Nogueira <fabricionogueira@CERCOMP-608830.(none)>
2013-08-05 10:55:52 -0300

    Definindo scritp para instalação dos programas padrões

---
+++ default-apps.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# Debian - Sistemas padrões."
+echo "Iniciando a instalação..."
+apt-get install alien isomaster lame limewire-basic mplayer  qcad skype twitux wine php5 apache2 mysql postgres terminator htop 
+echo "Finalizado!"
