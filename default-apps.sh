fa38e2e37d01e75f663cd93ecc38b2b0f71c4e81
Fabricio Nogueira <fabricionogueira@CERCOMP-608830.(none)>
2013-08-05 11:08:23 -0300

    Definindo scritp para instalação dos programas padrões

--- default-apps.sh
+++ default-apps.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Debian - Sistemas padrões."
 echo "Iniciando a instalação..."
-apt-get install alien isomaster lame limewire-basic mplayer  qcad skype twitux wine php5 apache2 mysql postgres terminator htop 
+apt-get install alien wine faad lame php5 php5-cgi php5-cli php5-common php5-curl php5-gd php5-mysql php5-odbc php5-pgsql php5-xdebug php-mail php-mail-mime apache2 apache2-utils mysql-client mysql-common mysql-server mysql-workbench mysql-workbench-data postgresql-9.1 postgresql-client-9.1 postgresql-server-dev-all terminator htop 
 echo "Finalizado!"
